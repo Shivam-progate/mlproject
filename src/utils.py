b555540bead57fb9dfc1830ba7e6437c0ce544d6
Shivam-progate <shivam.sharma12302@gmail.com>
2023-03-19 18:30:13 +0530

    Flask

--- src/utils.py
+++ src/utils.py
@@ -3,9 +3,10 @@ import sys
 
 import numpy as np 
 import pandas as pd
-import dill 
+import dill
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
+
 from src.exception import CustomException
 
 def save_object(file_path, obj):
@@ -19,6 +20,7 @@ def save_object(file_path, obj):
 
     except Exception as e:
         raise CustomException(e, sys)
+    
 def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     try:
         report = {}
@@ -48,4 +50,12 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
         return report
 
     except Exception as e:
-        raise CustomException(e, sys)    
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
